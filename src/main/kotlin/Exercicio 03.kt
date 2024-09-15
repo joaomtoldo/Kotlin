1fe125361b9b638ef362ed9f8357c377c79909bd
João Toldo <joao.toldo@protonmail.com>
2024-09-14 22:20:13 -0300

    2 Commit

--- src/main/kotlin/Exercicio 03.kt
+++ src/main/kotlin/Exercicio 03.kt
@@ -24,4 +24,62 @@ fun main(){
     println("É estudante?: $isStudent")
 
 
+
+
+
+//     CONVERSÃO ENTRE TIPOS DE DADOS:
+
+    val numero: Int = 10
+    val double: Double = numero.toDouble() //Converte de Int para Double
+    println("Número inteiro: $numero")
+    println("Número decimal: $double")
+
+    val text: String = "2024"
+    val convertedNum: Int = text.toInt() //Converte string para inteiro
+    println("texto convertido para inteiro: $convertedNum")
+
+
+//    Usando operações com diferentes tipos de dados
+
+    val num01: Int = 10
+    val num02: Double = 5.5
+    val resultado: Double = num01 + num02
+
+    println("A soma de $num01 e $num02 é $resultado")
+
+//    Declaração de constantes usando val e variaves mutáveis usando var
+
+    val pi: Double = 3.14159 // constante não pode ser alterada
+    var raio: Double = 2.0 // variavel mutável
+
+    var areaCirculo: Double = pi * raio * raio
+    println("A área do circulo é $areaCirculo")
+//    mudando o valor do raio
+    raio = 3.0
+    var newArea: Double = pi * raio * raio
+    println("A nova área do circulo é $newArea")
+
+
+//    Entrada de dados do usuário e uso de tipos de dados:
+
+    print("Digite sua idade: ")
+    val idade = readLine()?.toIntOrNull() ?: 0 // lê a idade como string e converte para Int
+
+    println("Digite sua altura: ")
+    val altura = readLine()?.toDoubleOrNull() ?: 0.0 // lê a altura como string e converte para Int
+
+    if(idade > 0 && altura > 0.0){
+        println("Sua idade é $idade e sua altura é $altura")
+    }else{
+        println("Entrada inválida")
+    }
+
 }
+/*
+
+Explicação:
+readLine(): Lê uma linha de texto do teclado.
+toIntOrNull(), toDoubleOrNull(): Tentam converter a entrada para Int ou Double. Se a conversão falhar,
+retorna null. O operador ?: define um valor padrão em caso de falha.
+Esses exemplos abordam tipos básicos de dados e conversões em Kotlin,
+além de introduzir o uso de entradas de dados e variáveis imutáveis/mutáveis.*/
